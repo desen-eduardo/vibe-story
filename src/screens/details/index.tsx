5a100fe9627c42d943af71c349ce15b31c451a88
eduardo <angeloeduardo_184@yahoo.com.br>
2026-04-25 18:29:34 -0300

    criado a tela de home e detalhes

--- src/screens/details/index.tsx
+++ src/screens/details/index.tsx
@@ -1,8 +1,10 @@
 import { Text, View } from 'react-native';
+import Header from '../../components/header';
 
 export default function Details() {
     return(
         <View>
+            <Header />
             <Text>Details</Text>
         </View>
     )
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text } from 'react-native';
+import Header from '../../components/header';
+import Product from '../../components/product';
+
+const produtos = [1,2,3,4,5];
 
 export default function Home() {
-    return(
-        <View>
-            <Text>Home</Text>
-        </View>
+    return (
+        <>
+            <Header />
+            <Text style={style.titleProduct}>Listagem de Produtos</Text>
+            <FlatList 
+                data={produtos}
+                renderItem={({item}) => <Product />}
+                numColumns={2}
+            />
+        </>
     )
 }
+
+const style = StyleSheet.create({
+    titleProduct:{
+        fontFamily:'Roboto-Bold',
+        fontSize:24,
+        paddingVertical:10,
+        paddingHorizontal:10
+    }
+})
+
